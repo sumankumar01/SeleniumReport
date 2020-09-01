d6f5f00eced08c099837fd82a92353f80424bc37
Suman kumar <32861501+sumankumar01@users.noreply.github.com>
2020-09-01 21:31:17 +0530

    2nd version

--- src/test/java/Runner/run.java
+++ src/test/java/Runner/run.java
@@ -2,20 +2,11 @@ package Runner;
 
 import org.testng.annotations.Test;
 
-import com.thoughtworks.selenium.FlashSelenium;
-import com.thoughtworks.selenium.FlexUISelenium;
 
-import Flash.FlashObjectWebDriver;
 import customReport.Report;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
